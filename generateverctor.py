c9a3382a01a86a7836b524010681e1b92fcfe919
zpole <zhi951206@gmail.com>
2019-03-11 00:58:41 +0000

    Correct spelling mistakes

--- generateverctor.py
+++ generateverctor.py
@@ -4,15 +4,15 @@ import glob
 
 
 def readfile():
-    contants = []
+    contents = []
     labels = []
-    uni = []
+    uni = []  # university index
     allfiles = glob.glob('processed/**/*', recursive=True)
     for line in allfiles:
         try:
             with open(os.path.abspath(line)) as file:
                 data = file.read().replace("\n", " ")
-                contants.append(data)
+                contents.append(data)
         except:
             continue
         tags = os.path.normpath(line).split(os.sep)
@@ -20,26 +20,26 @@ def readfile():
         labels.append(tags[1])
         # add university for each file
         uni.append(tags[2])
-    return contants, labels, uni
+    return contents, labels, uni
 
 
 def tfidf():
-    contants, labels, uni = readfile()
+    contents, labels, uni = readfile()
     tfidf = TfidfVectorizer(stop_words="english")
-    vectors = tfidf.fit_transform(contants)
+    vectors = tfidf.fit_transform(contents)
     return vectors, labels, uni, tfidf.get_feature_names()  # uni = university
 
 
 def bow():
-    contants, labels, uni = readfile()
+    contents, labels, uni = readfile()
     bow = CountVectorizer(stop_words="english")
-    vectors = bow.fit_transform(contants)
+    vectors = bow.fit_transform(contents)
     return vectors, labels, uni, bow.get_feature_names()
 
 
 # test
 if __name__ == '__main__':
-    # contants, labels = readfile("testpath.txt")
-    # print(contants[1], labels[1])
+    # contents, labels = readfile()
+    # print(contents[1], labels[1])
     vectors, labels, uni, features = tfidf()
     print(vectors.shape, len(labels), len(uni))
