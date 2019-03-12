f2ac616f8459059274747f6ccd8483ab753b8e65
Chito Wong <9390786+wongchito@users.noreply.github.com>
2019-03-12 18:03:03 +0000

    Fix bugs

--- classification.py
+++ classification.py
@@ -98,7 +98,7 @@ def lrclassifier(train_vector, train_label, test_vector):
 
 if __name__ == '__main__':
     classes = ["course", "department", "faculty", "other", "project", "staff", "student"]
-    vectors, labels, uni, features = generateverctor.tfidf()
+    vectors, labels, uni, filename, features = generateverctor.tfidf()
     vectors = svd(vectors)
     train_vector, train_label, test_vector, test_label = splitvector(vectors, labels, uni, "cornell")
     # predict = svmclassfier(train_vector, train_label, test_vector)
