cd04789557e940afc39a9fa34234b38c65d8a418
Chito Wong <9390786+wongchito@users.noreply.github.com>
2019-03-12 19:16:32 +0000

    Update rules

--- preprocess/textprepro.py
+++ preprocess/textprepro.py
@@ -3,5 +3,6 @@ import re
 def pp(data):
     # data = data.replace("\n", " ")
     data = data.lower()
-    data = re.sub(r'ph\.?d\.?', 'phd', data)
+    data = ' ' + data
+    data = re.sub(r'[^\w.]ph\.? ?d\.?', ' phd', data)
     return data
