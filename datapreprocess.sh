8137efcf0b49d7c07c8d518a2fc5fdde04e049b0
wongchito <9390786+wongchito@users.noreply.github.com>
2019-04-05 13:49:15 +0100

    Remove unused codes

--- datapreprocess.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf tokens*
-rm -f data.zip
-source activate dme
-python preprocess/parse.py --stem False --stop False
-python preprocess/parse.py --stem False --stop True
-python preprocess/parse.py --stem True --stop False
-python preprocess/parse.py --stem True --stop True
-zip -r data.zip tokens* webkb
